5c7f5168c4b05493f5bb18ddab9cf371d95ae391
tiiedye <tiiedye@gmail.com>
2022-01-03 09:31:40 -0800

    creating window and adding canvas to it

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(pady=20, padx=20, bg="white")
+
+canvas = Canvas(height=200, width=200, bg="white", highlightthickness=0)
+img = PhotoImage(file="./logo.png")
+canvas.create_image(100, 100, image=img)
+canvas.grid(column=1, row=1)
+
+# All code before exit
+window.mainloop()
