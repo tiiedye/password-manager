95db2efca9d08208724714afe23f92de9439165a
tiiedye <tiiedye@gmail.com>
2022-01-03 10:34:09 -0800

    adding save functionality

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from tkinter import *
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_txt.get()
+    user = username_txt.get()
+    password = password_txt.get()
+
+    with open("./data.txt", "a") as file:
+        file.write(f"{website} | {user} | {password}\n")
+
+    website_txt.delete(0, END)
+    username_txt.delete(0, END)
+    password_txt.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -17,11 +30,13 @@ canvas.grid(column=1, row=0)
 website_lbl = Label(text="Website:")
 website_lbl.grid(column=0, row=1)
 website_txt = Entry(width=54)
+website_txt.focus()
 website_txt.grid(column=1, row=1, columnspan=2)
 
 username_lbl = Label(text="Email/Username:")
 username_lbl.grid(column=0, row=2)
 username_txt = Entry(width=54)
+username_txt.insert(0, "example@gmail.com")
 username_txt.grid(column=1, row=2, columnspan=2)
 
 password_lbl = Label(text="Password:")
@@ -32,7 +47,7 @@ password_txt.grid(column=1, row=3)
 generate_pass_btn = Button(text="Generate Password")
 generate_pass_btn.grid(column=2, row=3)
 
-add_btn = Button(text="Add", width=46)
+add_btn = Button(text="Add", width=46, command=save)
 add_btn.grid(column=1, row=4, columnspan=2)
 
 # All code before exit
